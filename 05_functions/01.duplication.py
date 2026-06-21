837480015fff920637ce70fab1327c50da691ddb
arjunkumar811 <kumararjun26401@gmail.com>
2026-06-21 20:17:20 +0530

    funtions

---
+++ 05_functions/01.duplication.py
@@ -0,0 +1,5 @@
+def print_order(name, surename, age):
+    print(f"My Name is {name} and surename is {surename} and I am {age} years old.")
+print_order("Arjun", "Itagi", "21");
+print_order("Prakash", "patil", "21");
+print_order("ambrish", "kalshetty", "21");
---
+++ 05_functions/splitting.py
@@ -0,0 +1,16 @@
+def generate_report():
+    fetch_sales();
+    filter_valid_order();
+    summarize_data();
+
+
+def fetch_sales():
+    print("fetch_sales");
+
+def filter_valid_order():
+    print("filter_valid_order");
+
+def summarize_data():
+    print("summarize_data");
+
+generate_report();
