5a9942746a306c001157b33914cedacfd5e10459
arjunkumar811 <kumararjun26401@gmail.com>
2026-06-23 22:22:29 +0530

    generators

---
+++ 08_generators/01_basics.py
@@ -0,0 +1,28 @@
+# A generator in Python is a special type of function that produces values one at a time instead of returning all values at once.
+
+# It uses the yield keyword instead of return.
+
+def serve_chai():
+    yield "Cup 1: Masala Chai"
+    yield "Cup 2: Ginger Chai"
+    yield "Cup 3: Elaichi Chai"
+
+stall = serve_chai()
+
+# for cup in stall:
+#     print(cup)
+
+def get_chai_list():
+    return ["Cup 1", "Cup 2", "Cup 3"]
+
+# generator function
+def get_chai_gen():
+    yield "Cup 1"
+    yield "Cup 2"
+    yield "Cup 3"
+
+chai = get_chai_gen()
+print(next(chai))
+print(next(chai))
+print(next(chai))
+# print(next(chai)) # gives error
