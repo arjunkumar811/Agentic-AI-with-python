f6c51c78a99e7954f75a712c9b2cbadddee97929
arjunkumar811 <kumararjun26401@gmail.com>
2026-09-11 23:15:24 +0530

    Added Multi-mordel

---
+++ image/main.py
@@ -0,0 +1,24 @@
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv()
+
+client = OpenAI()
+
+response = client.chat.completions.create(
+    model="gpt-4. 1-mini",
+    message=[
+        {
+            "role": "user",
+            "content": [
+                { "type": "text, "text": "Generate a caption for this image in about 40 words"},
+                {"type": "image_url", "image_url": {"url": "............."}}
+
+            ]
+
+        }
+    ]
+)
+
+
+print("Response:", response.choices[0].message.content)
